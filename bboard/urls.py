b741ed47b0c1ba24c76323e804ed23084de7e743
Давид Галенков <@itstep.loc>
2023-06-12 17:01:04 +0600

    lesson_015

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 
-from bboard.views import index, by_rubric, BbCreateView
+from bboard.views import index, by_rubric, BbCreateView, add_save, add, add_and_save
 
 # app_name = 'bboard'
 
@@ -9,14 +9,17 @@ vals = {
     'beaver': 'beaver – это бобёр!'
 }
 
-# urlpatterns = [
-#     path('', index, name='index'),
-#     path('<int:rubric_id>/', by_rubric, vals, name='by_rubric'),
-#     path('add/', BbCreateView.as_view(), name='add'),
-# ]
-
 urlpatterns = [
-    re_path(r'^$', index, name='index'),
-    re_path(r'^(?P<rubric_id>[0-9]*)/$', by_rubric, vals, name='by_rubric'),
-    re_path(r'^add/$', BbCreateView.as_view(), name='add'),
+    path('', index, name='index'),
+    path('<int:rubric_id>/', by_rubric, vals, name='by_rubric'),
+    # path('add/', BbCreateView.as_view(), name='add'),
+    # path('add/save/', add_save, name='add_save'),
+    # path('add/', add, name='add'),
+    path('add/', add_and_save, name='add'),
 ]
+
+# urlpatterns = [
+#     re_path(r'^$', index, name='index'),
+#     re_path(r'^(?P<rubric_id>[0-9]*)/$', by_rubric, vals, name='by_rubric'),
+#     re_path(r'^add/$', BbCreateView.as_view(), name='add'),
+# ]
